257624c47ccd4d96df0dddb9adcc4dab78fefe26
Anuj Datar <anuj.datar@gmail.com>
2020-04-09 07:50:07 -0400

    extracts specified archive contents to /tmp/extracted

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,23 +1,37 @@
 #! /bin/bash
 
-# The ex function from .bashrc in Manjaro
-# I did not write this..
+# This is the ex function from .bashrc in Manjaro Linux
+# I did not write this function.
+  ex() {
+    if [ -f $1 ] ; then
+    case $1 in
+      *.tar.bz2)   tar xjf $1;;
+      *.tar.gz)    tar xzf $1;;
+      *.bz2)       bunzip2 $1;;
+      *.rar)       unrar x $1;;
+      *.gz)        gunzip $1;;
+      *.tar)       tar xf $1;;
+      *.tbz2)      tar xjf $1;;
+      *.tgz)       tar xzf $1;;
+      *.zip)       unzip $1;;
+      *.Z)         uncompress $1;;
+      *.7z)        7z x $1;;
+      *)           echo "'$1' not compatible, unable to extract archive" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+  }
 
-if [ -f $1 ] ; then
-  case $1 in
-    *.tar.bz2)   tar xjf $1   ;;
-    *.tar.gz)    tar xzf $1   ;;
-    *.bz2)       bunzip2 $1   ;;
-    *.rar)       echo "unrar unavailable, RAR archives not compatible";;
-    *.gz)        gunzip $1    ;;
-    *.tar)       tar xf $1    ;;
-    *.tbz2)      tar xjf $1   ;;
-    *.tgz)       tar xzf $1   ;;
-    *.zip)       echo "unzip unavailable, ZIP archives not compatible";;
-    *.Z)         uncompress $1;;
-    *.7z)        echo "7z unavailable, 7z archives not compatible";;
-    *)           echo "'$1' not compatible, unable to extract archive" ;;
-  esac
-else
-  echo "'$1' is not a valid file"
-fi
+# Save current dir
+CURRENT_DIR=$(pwd)
+
+# make temp directory
+mkdir -p /tmp/extracted
+
+# extract into target temp dir
+cd /tmp/extracted
+ex $1
+
+# go back to current dir
+cd $pwd
