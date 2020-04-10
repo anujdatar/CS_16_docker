d08e9cc36810901c4bdc3e437ae180330851c8db
Anuj Datar <anuj.datar@gmail.com>
2020-04-10 12:39:35 -0400

    added metamod installation check to amx and podbot install

--- cs_server.sh
+++ cs_server.sh
@@ -53,19 +53,20 @@ install() {
 
   # Install Untitled maps
   echo "Installing Untitled map pack"
-  if [ -f ~/addons/cs_untitld_1_2.tar.gz ]; then
-    tar xzf ~/addons/cs_untitld_1_2.tar.gz -C $HLDS_DIR
+  if [ -f ~/addons/cs_untitled_1_2.tar.gz ]; then
+    tar xzf ~/addons/cs_untitled_1_2.tar.gz -C $HLDS_DIR
   else
-    echo "Unable to locate $HOME/addons/cs_untitld_1_2.tar.gz"
+    echo "Unable to locate $HOME/addons/cs_untitled_1_2.tar.gz"
     echo "Please verify map archive and manually copy to server"
   fi
 
   echo "Installation complete"
+  return 0
 }
 
 install_metamod() {
   # check if metamod is installed
-  [ -d $HLDS_DIR/cstrike/addons/metamod ] && echo "MetaMod already installed" && return
+  [ -d $HLDS_DIR/cstrike/addons/metamod ] && echo "MetaMod already installed" && return 0
   
   echo "Installing MetaMod on game server"
   if [ -f ~/addons/metamod-1.21.1.tar.gz ]; then
@@ -74,20 +75,36 @@ install_metamod() {
     tar xzf ~/addons/metamod-1.21.1.tar.gz -C $HLDS_DIR/cstrike/addons/
     # copy liblist.gam to enable metamod on game server
     cp -fa ~/addons/liblist.gam $HLDS_DIR/cstrike/liblist.gam
+    echo "MetaMod install complete"
   else 
     echo "Unable to locate $HOME/addons/metamod-1.21.1.tar.gz"
     echo "Please verify mod archive and manually copy to server"
     return 1
   fi
+  return 0
+}
+
+check_metamod_install() {
+  echo "Checking MetaMod installation"
+  if [ -d $HLDS_DIR/cstrike/addons/metamod ]; then
+    echo "MetaMod OK"
+    return 0
+  else
+    echo "MetaMod not installed"
+    install_metamod
+    return
+  fi
 }
 
 install_amxmodx() {
   # check if amxmodx is installed
-  [ -d $HLDS_DIR/cstrike/addons/amxmodx ] && echo "AMXmodX MM already installed" && return
-  # check metamod install
-  [ ! -d $HLDS_DIR/cstrike/addons/metamod ] && echo "MetaMod not installed" && install_metamod
-  # check if metamod install was successful
-  [ $? -eq 1 ] && echo "MetaMod Install unsuccessful. Unable to install AMXmodX" && return 1
+  [ -d $HLDS_DIR/cstrike/addons/amxmodx ] && echo "AMXmodX MM already installed" && return 0
+
+  check_metamod_install
+  if [ $? -eq 1 ]; then
+    echo "MetaMod Install unsuccessful. Unable to install AMXmodX"
+    return 1
+  fi
 
   echo "Installing AMXmodX MetaMod on game server"
   if [ -f ~/addons/amxmodx_cs-1.8.2_linux.tar.gz ]; then
@@ -100,29 +117,35 @@ install_amxmodx() {
     echo "Please verify mod archive and manually copy to server"
     return 1
   fi
+  echo "AMXmodX install complete"
+  return 0
 }
 
 install_podbot() {
   # check if podbot is installed
-  [ -d $HLDS_DIR/cstrike/addons/podbot ] && echo "POD-bot MM already installed" && return
-  # check metamod install
-  [ ! -d $HLDS_DIR/cstrike/addons/metamod ] && echo "MetaMod not installed" && install_metamod
-  # check if metamod install was successful
-  [ $? -eq 1 ] && echo "MetaMod Install unsuccessful. Unable to install PoDBot" && return 1
-  
-  echo "Install POD-bot MetaMod on game server"
+  [ -d $HLDS_DIR/cstrike/addons/podbot ] && echo "POD-bot MM already installed" && return 0
+
+  check_metamod_install
+  if [ $? -eq 1 ]; then
+    echo "MetaMod Install unsuccessful. Unable to install AMXmodX"
+    return 1
+  fi
+
+  echo "Install PoDBot MetaMod on game server"
   if [ -f ~/addons/podbot-3.0_22.tar.gz ]; then
     # extract podbot
     tar xzf ~/addons/podbot-3.0_22.tar.gz -C $HLDS_DIR/cstrike/addons/
     # add podbot to metamod plugins list
     echo "linux addons/podbot/podbot_mm_i386.so" >> $HLDS_DIR/cstrike/addons/metamod/plugins.ini
     # copy extra waypoints
-    tar xzf ~/addons/waypoints.tar.gz -C $HLDS_DIR/cstrike/addons/podbot/\
+    tar xzf ~/addons/waypoints.tar.gz -C $HLDS_DIR/cstrike/addons/podbot/
   else 
     echo "Unable to locate $HOME/addons/podbot-3.0_22.tar.gz"
     echo "Please verify mod archive and manually copy to server"
     return 1
   fi
+  echo "PoDBot install complete"
+  return 0
 }
 
 update() {
@@ -137,7 +160,7 @@ start() {
   [ ! -d "$HLDS_DIR/cstrike" ] && install || echo "Counter Strike 1.6 Dedicated Server installed"
   
   # check if hlds is running
-  [ -n "$(pidof hlds_run)" ] && echo "Counter Strike 1.6 Dedicated Server is already running on port:$PORT" && return
+  [ -n "$(pidof hlds_run)" ] && echo "Counter Strike 1.6 Dedicated Server is already running on port:$PORT" && return 1
 
   # load server constants
   [ -f ~/store/constants.sh ] && source ~/store/constants.sh || true
@@ -151,7 +174,7 @@ start() {
 
 stop() {
   # check if hlds is running
-  [ -z "$(pidof hlds_linux)" ] && echo "Counter Strike 1.6 Dedicated Server not running" && return
+  [ -z "$(pidof hlds_linux)" ] && echo "Counter Strike 1.6 Dedicated Server not running" && return 1
   pkill hlds_linux
   pkill hlds_run
   store_config
@@ -161,7 +184,7 @@ stop() {
 
 restart() {
   # check if hlds is running
-  [ -z "$(pidof hlds_linux)" ] && echo "Counter Strike 1.6 Dedicated Server not running" && return
+  [ -z "$(pidof hlds_linux)" ] && echo "Counter Strike 1.6 Dedicated Server not running" && return 1
   echo "Restarting Counter Strike 1.6 Dedicated Server"
   store_config
   pkill hlds_linux
