962e184ac094d122a81ca366b738bfe70ff24680
Anuj Datar <anuj.datar@gmail.com>
2020-04-10 15:36:38 -0400

    script now uses the extract function

--- scripts/copy_folder_to_cstrike.sh
+++ scripts/copy_folder_to_cstrike.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
-cp -r maps $HLDS_DIR/cstrike/
+# declare directory for extraction of archive
+export TARGET_DIR="/tmp/extracted"
+
+# extract archive to target dir
+./extract.sh $1
+
+if [ $? -eq 1 ]; then
+  echo "Archive extraction failed"
+  exit 1
+fi
+
+# copy contents of extracted archive to $HLDS_DIR/cstrike folder
+cp -r $TARGET_DIR/cstrike/* $HLDS_DIR/cstrike
+
+# remove extracted archive contents and host dir
+rm -rf $TARGET_DIR
