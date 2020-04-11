cb05e4a97c337126f09e66410d81ff61109dd9aa
Anuj Datar <anuj.datar@gmail.com>
2020-04-11 19:58:58 -0400

    removed ~/bin, copy main scripts to /bin/, no server install -> smaller image

--- bin/cleanup.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-rm -r /home/steam/addons
--- bin/install_cstrike.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cd $STEAMCMD_DIR
-echo "Installing Counter Strike 1.6 Dedicated server"
-# install hlds and cs server, needs to be done more than once sometimes
-./steamcmd.sh +login anonymous +force_install_dir $HLDS_DIR +app_set_config 90 mod cstrike +app_update 90 validate +quit || true
-./steamcmd.sh +login anonymous +force_install_dir $HLDS_DIR +app_set_config 90 mod cstrike +app_update 90 validate +quit || true
-
-echo "Installation complete"
